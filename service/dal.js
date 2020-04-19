5232b69653e107b5ebd7fe5aad85d3bf28ea35ae
jose.silas.c.vieira@accenture.com <jose.silas.c.vieira@accenture.com>
2020-04-18 21:32:35 -0300

    Nova API de Inserir Clientes

--- service/dal.js
+++ service/dal.js
@@ -5,10 +5,111 @@ class DataAcessLayer {
         const bodyParser = require('body-parser');
         const mysql = require('mysql');
         const cors = require('cors');
-
     };
 
+    insertLoginTransaction(req,pool) {
+        // Parâmetros para TABELA-LOGIN
+        let cd_login_request = req.param("cd_login");
+        let cd_senha_request = req.param("cd_senha");
+        let sn_ativo_login = 1;
+        let tp_login = 'cliente'
+        //Query para inserir na tabela Login
+        let query = 'INSERT INTO LOGIN VALUES ('
+            + 'NULL,'
+            + "'" + cd_login_request + "'" + ','
+            + "'" + cd_senha_request + "'" + ','
+            + sn_ativo_login + ','
+            + "'" + tp_login + "'" + ')'
 
+        //Baseado no resultado da promessa irá mandar uma mensagem de retorno e será enviado como retorno da aplicação(Resolve).
+        return new Promise((resolve, reject) => {
+
+            //Iniciando Transação
+            pool.beginTransaction(function (err) {
+                if (err) {
+                    throw err;
+                }
+                pool.query(query, function (error, results, fields) {
+                    if (!error) {
+                        //Retorno do MYSQL com o ÚLTIMO ID ADICIONADO NA TABELA LOGIN
+                        var id_login = results.insertId;
+                        // Variavéis do Request
+                        let nome = req.param("nome");
+                        let email = req.param("email");
+                        let endereco = req.param("endereco");
+                        let sn_ativo = 1;
+                        let telefone = req.param("telefone");
+                        let celular = req.param("celular");
+                        //Query para inserir na tabela CLIENTE
+                        query = 'INSERT INTO CLIENTE VALUES ('
+                            + 'NULL,'
+                            + "'" + id_login + "'" + ","
+                            + "'" + nome + "'" + ','
+                            + "'" + email + "'" + ','
+                            + "'" + endereco + "'" + ','
+                            + sn_ativo + ','
+                            + "'" + telefone + "'" + ','
+                            + "'" + celular + "'" + ')'
+                        
+                        pool.query(query, function (error, results, fields) {
+                            //Se a query de inserção não falhar irá realizar o commit da operação
+                            if (!error) {
+                                pool.commit(function (err) {
+                                    //Caso apresente algua falha no commit irá enviar essa informação.
+                                    if (err) {
+                                        return pool.rollback(function () {
+                                            var resultado_cliente = JSON.parse('{"status":"Falha ao realizar o cadastro tenta novamente","code_status":"00"}');
+                                            resolve(resultado_cliente);
+                                        });
+                                    }else{
+                                    //Finalização do cadastro.
+                                    var resultado_cliente = JSON.parse('{"status":"Cliente Cadastrado com Sucesso","code_status":"01"}');
+                                    resolve(resultado_cliente);
+                                }
+                                });
+
+                            } else if (error.code === 'ER_DUP_ENTRY') {
+                                //Captura de erros e retornos para api
+                                return pool.rollback(function () {
+                                    var resultado_cliente = JSON.parse('{"status":"Já existe um Cliente com esse E-mail Cadastrado","code_status":"02"}');
+                                    resolve(resultado_cliente);
+                                });
+                            } else if (error.code === 'ER_DATA_TOO_LONG') {
+                                //Captura de erros e retornos para api
+                                var resultado = JSON.parse('{"status":"Coluna com o valor maior do que o permitido","code_status":"03"}');
+                                resolve(resultado);
+                            } else {
+                                var resultado = JSON.parse('{"status":"Operação desconhecida, entre em contato com TI","code_status":"XX"}');
+                                resolve(resultado);
+
+                            }
+
+                        });
+
+
+
+
+                    } else if (error.code === 'ER_DUP_ENTRY') {
+                        return pool.rollback(function () {
+                            var resultado = JSON.parse('{"status":"Já existe um usuário com esse Login","code_status":"02"}');
+                            resolve(resultado);
+                        });
+                    } else {
+                        var resultado = JSON.parse('{"status":"Operação desconhecida, necessário debugar o código","code_status":"XX"}');
+                        resolve(resultado);
+
+                    }
+
+                    pool.release();
+                });
+
+
+
+            });
+
+        });
+
+    }
 
     insertLogin(req, res, pool) {
 
@@ -25,51 +126,55 @@ class DataAcessLayer {
             + sn_ativo_login + ','
             + "'" + tp_login + "'" + ')'
 
-            console.log(query);
+        console.log(query);
 
 
-            try{
-
-            
-
-        return new Promise((resolve, reject) => {
-            pool.query(query, res, function (err, results, fields) {
-                if (!err) {
-                    var resultado = JSON.parse('{"status":"Login Criado com Sucesso","code_status":"01"}');
-                    resolve(results.insertId);
-                    
-                    
-
-                } else if (err.code === 'ER_DUP_ENTRY') {
-                    var resultado = JSON.parse('{"status":"Já existe um usuário com esse ID","code_status":"02"}');
-                    pool.release();
-                    resolve(resultado);
-                    
-
-                } else if (err.code = 'ER_DATA_TOO_LONG'){
-                    
-                    var resultado = JSON.parse('{"status":"Coluna com o valor maior do que o permitido","code_status":"03"}');
-                    pool.release();
-                    resolve(resultado);
-
-                }else{
-                    pool.release();
-                    reject(new Error('woops')).catch(error => {
-                        console.log('ERRROOOOOOOOOOOO')
-
-                    });
-
-                }
-            });
+        try {
 
 
-        })
 
-    }catch(err){
-        console.log('ERRRRO DOS GRANDES.')
-        pool.release();
+            return new Promise((resolve, reject) => {
+                pool.query(query, res, function (err, results, fields) {
+                    if (!err) {
+                        var resultado = JSON.parse('{"status":"Login Criado com Sucesso","code_status":"01"}');
+                        resolve(results.insertId);
 
-    }
+
+
+                    } else if (err.code === 'ER_DUP_ENTRY') {
+                        var resultado = JSON.parse('{"status":"Já existe um usuário com esse ID","code_status":"02"}');
+                        pool.rollback(function () {
+                            pool.release();
+                            resolve(resultado);
+
+                        });
+
+
+
+                    } else if (err.code = 'ER_DATA_TOO_LONG') {
+
+                        var resultado = JSON.parse('{"status":"Coluna com o valor maior do que o permitido","code_status":"03"}');
+                        pool.release();
+                        resolve(resultado);
+
+                    } else {
+                        pool.release();
+                        reject(new Error('woops')).catch(error => {
+                            console.log('ERRROOOOOOOOOOOO')
+
+                        });
+
+                    }
+                });
+
+
+            })
+
+        } catch (err) {
+            console.log('ERRRRO DOS GRANDES.')
+            pool.release();
+
+        }
 
     }
 
@@ -93,7 +198,7 @@ class DataAcessLayer {
             + "'" + telefone + "'" + ','
             + "'" + celular + "'" + ')'
 
-            console.log('QUERRRYYYYYYYY----->' + query);
+        console.log('QUERRRYYYYYYYY----->' + query);
 
         return new Promise((resolve, reject) => {
             pool.query(query, res, function (err, results, fields) {
@@ -101,12 +206,15 @@ class DataAcessLayer {
                     var resultado_cliente = JSON.parse('{"status":"Cliente Cadastrado com Sucesso","code_status":"01"}');
                     resolve(resultado_cliente);
 
-                    
+
 
                 } else if (err.code === 'ER_DUP_ENTRY') {
                     var resultado_cliente = JSON.parse('{"status":"Já existe um Cliente com esse E-mail Cadastrado","code_status":"02"}');
-                    pool.release();
-                    resolve(resultado_cliente);
+                    pool.rollback(function () {
+                        pool.release();
+                        resolve(resultado_cliente);
+
+                    });
                 } else {
                     pool.release();
                     reject(err);
--- service/index.js
+++ service/index.js
@@ -33,46 +33,71 @@ app.use('/', router);
 app.post('/addCliente', function (req, res) {
     //Abrindo Conexão com o Banco de Dados (Objeto Pool Importado da Classe DataBase)
     pool.getConnection(function (err, pool) {
-        
         const objeto_dal = new DataAcessLayer();
-
-        try{
-        async function realizarCadastro() {
-            const resultados_login = await objeto_dal.insertLogin(req, res, pool).then(resultado => {
-
-                console.log('RESULTADO DO RETORNO DO LOGIN - ' + resultado);
-                return resultado;
-            });
-
-            if (Number(resultados_login.code_status) === 2) {
-                res.send(resultados_login);
-                return;
-            } else {
-                const resultado = await objeto_dal.insertCliente(req, res, pool, resultados_login).then(resultado_cliente => {
-                    if (typeof (resultado_cliente) !== 'number') {
-                        res.send(resultado_cliente);
-                    } else {
-                        res.send(resultado_cliente)
-                    }
+        try {
+            async function realizarCadastro() {
+                await objeto_dal.insertLoginTransaction(req,pool).then(resultado => {
+                    res.send(resultado);
                 });
             }
 
-        }
-
         realizarCadastro();
         
+        } catch (err) {
+            var error_status = JSON.parse('{"status":"Não foi possível realizar sua operação, entre em contato com o administrador.","code_status":"00"}');
+            res.send(error_status);
 
-    }catch(err){
-        console.log('ERRRRO DOS GRANDES.')
+        }
+    });
+});
 
-    }
+
+
+
+
+/*app.post('/addClienteOLD', function (req, res) {
+    //Abrindo Conexão com o Banco de Dados (Objeto Pool Importado da Classe DataBase)
+    pool.getConnection(function (err, pool) {
+
+        const objeto_dal = new DataAcessLayer();
+
+        try {
+            async function realizarCadastro() {
+                const resultados_login = await objeto_dal.insertLogin(req, res, pool).then(resultado => {
+
+                    console.log('RESULTADO DO RETORNO DO LOGIN - ' + resultado);
+                    return resultado;
+                });
+
+                if (Number(resultados_login.code_status) === 2) {
+                    res.send(resultados_login);
+                    return;
+                } else {
+                    const resultado = await objeto_dal.insertCliente(req, res, pool, resultados_login).then(resultado_cliente => {
+                        if (typeof (resultado_cliente) !== 'number') {
+                            res.send(resultado_cliente);
+                        } else {
+                            res.send(resultado_cliente)
+                        }
+                    });
+                }
+
+            }
+
+            realizarCadastro();
+
+
+        } catch (err) {
+            console.log('ERRRRO DOS GRANDES.')
+
+        }
 
 
     });
 
 
 
-});
+}); */
 
 
 // -------------SEÇÃO DE API PARA CLIENTE-----------------
