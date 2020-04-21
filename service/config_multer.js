5dcbe686a5aec086b22247b0af772ffd939eca8b
jose.silas.c.vieira@accenture.com <jose.silas.c.vieira@accenture.com>
2020-04-21 00:18:50 -0300

    Ajuste Multer

--- service/config_multer.js
+++ service/config_multer.js
@@ -2,7 +2,7 @@ var multer = require('multer');
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, __dirname + './')
+      cb(null, __dirname + '/')
     },
     filename: function (req, file, cb) {
       cb(null,  Date.now()+ '-' + file.originalname)
