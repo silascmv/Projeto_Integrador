dcb02354dfeb0d5de7194b63c7e6aad21aefedfc
Glitch (silascmv-projeto-integrador) <none>
2020-04-22 02:33:59 +0000

    🌺🐪 Checkpoint ./service/index.js:14012742/15240 ./service/image_converter.js:14012742/517 ./service/database.js:14012742/1656 ./service/dal.js:14012742/13765 ./service/config_multer.js:14012742/677

--- service/config_multer.js
+++ service/config_multer.js
@@ -1,17 +1,14 @@
-var multer = require('multer');
+var multer = require("multer");
 
 var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, __dirname + '/tmp/my-uploads')
-    },
-    filename: function (req, file, cb) {
-      cb(null,  Date.now()+ '-' + file.originalname)
-    }
-  });
+  destination: function(req, file, cb) {
+    cb(null, __dirname + "/tmp/my-uploads");
+  },
+  filename: function(req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  }
+});
 
+const upload = multer({ storage: storage });
 
-  const upload = multer({storage: storage});
-
-  module.exports = upload;
-
-
+module.exports = upload;
--- service/database.js
+++ service/database.js
@@ -1,35 +1,33 @@
-var mysql = require('mysql');
-// node -v must > 8.x 
-var util = require('util');
-
+var mysql = require("mysql");
+// node -v must > 8.x
+var util = require("util");
 
 var pool = mysql.createPool({
-    connectionLimit: 10,
-    host: 'remotemysql.com',
-    user: 'wIp1MpiKOt',
-    password: 'pyiEqBNdgy',
-    database: 'wIp1MpiKOt',
-    connectionLimit: 10,
-    multipleStatements: true
+  connectionLimit: 10,
+  host: "remotemysql.com",
+  user: "wIp1MpiKOt",
+  password: "pyiEqBNdgy",
+  database: "wIp1MpiKOt",
+  connectionLimit: 10,
+  multipleStatements: true
 });
 
 pool.getConnection((err, connection) => {
-    if (err) {
-        
-        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-            console.error('Database connection was closed.')
-        }
-        if (err.code === 'ER_CON_COUNT_ERROR') {
-            console.error('Database has too many connections.')
-        }
-        if (err.code === 'ECONNREFUSED') {
-            console.error('Database connection was refused.')
-        }
+  if (err) {
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      console.error("Database connection was closed.");
     }
-    if (connection) connection.release()
-    return
-})
+    if (err.code === "ER_CON_COUNT_ERROR") {
+      console.error("Database has too many connections.");
+    }
+    if (err.code === "ECONNREFUSED") {
+      console.error("Database connection was refused.");
+    }
+  }
+  if (connection) connection.release();
+  return;
+});
 
 pool.query = util.promisify(pool.query);
 
-module.exports = pool
+module.exports = pool;
