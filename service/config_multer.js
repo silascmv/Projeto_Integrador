8e64281da67d03d851adc64c49d957db149ebf11
jose.silas.c.vieira@accenture.com <jose.silas.c.vieira@accenture.com>
2020-04-21 23:37:23 -0300

    Revert "Ajustes"
    This reverts commit c4396d35f6d7757cd10a4dc18a987b9b56b5c559, reversing
    changes made to dcb02354dfeb0d5de7194b63c7e6aad21aefedfc.

--- service/config_multer.js
+++ service/config_multer.js
@@ -1,7 +1,6 @@
 var multer = require("multer");
 
 var storage = multer.diskStorage({
-<<<<<<< HEAD
   destination: function(req, file, cb) {
     cb(null, __dirname + "/tmp/my-uploads");
   },
@@ -9,20 +8,6 @@ var storage = multer.diskStorage({
     cb(null, Date.now() + "-" + file.originalname);
   }
 });
-=======
-    destination: function (req, file, cb) {
-      cb(null, __dirname + '/my-uploads')
-    },
-    filename: function (req, file, cb) {
-      cb(null,  Date.now()+ '-' + file.originalname)
-    }
-  });
-
-
-  const upload = multer({storage: storage});
-
-  module.exports = upload;
->>>>>>> 12bd59101b9ec722ff902f35b71d932ebf8a9511
 
 const upload = multer({ storage: storage });
 
