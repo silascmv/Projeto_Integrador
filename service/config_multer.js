1841f6153c2df920a14cc89b38736503315c8150
jose.silas.c.vieira@accenture.com <jose.silas.c.vieira@accenture.com>
2020-04-22 00:02:06 -0300

    sasdas

--- service/config_multer.js
+++ service/config_multer.js
@@ -1,14 +1,27 @@
-var multer = require("multer");
+var multer = require('multer');
 
 var storage = multer.diskStorage({
+<<<<<<< HEAD
+    destination: function (req, file, cb) {
+      cb(null, __dirname + '/my-uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null,  Date.now()+ '-' + file.originalname)
+    }
+  });
+
+
+  const upload = multer({storage: storage});
+
+  module.exports = upload;
+=======
   destination: function(req, file, cb) {
-    cb(null, __dirname + "/tmp/my-uploads");
+    cb(null, __dirname + "/my-uploads");
   },
   filename: function(req, file, cb) {
     cb(null, Date.now() + "-" + file.originalname);
   }
 });
+>>>>>>> f653ef9738211481a72c7f99af5689e4e631bc3a
 
-const upload = multer({ storage: storage });
 
-module.exports = upload;
