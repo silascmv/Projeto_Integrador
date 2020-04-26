cfd1dbd9c9db391a0d85822ac1acd1722aa7b697
jose.silas.c.vieira@accenture.com <jose.silas.c.vieira@accenture.com>
2020-04-25 22:44:52 -0300

    Storage

--- service/config_multer.js
+++ service/config_multer.js
@@ -2,7 +2,7 @@ var multer = require('multer');
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, __dirname + '/')
+      cb(null, __dirname + '/my-uploads')
     },
     filename: function (req, file, cb) {
       cb(null,  req.param("NOME_PRODUTO") + '.jpg')
