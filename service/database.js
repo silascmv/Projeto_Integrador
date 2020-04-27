ddd74acc46677e309950778ee6e07b604d9872e1
jose.silas.c.vieira@accenture.com <jose.silas.c.vieira@accenture.com>
2020-04-27 18:39:31 -0300

    Ajuste

--- service/database.js
+++ service/database.js
@@ -5,10 +5,10 @@ var util = require('util');
 
 var pool = mysql.createPool({
     connectionLimit: 10,
-    host: 'remotemysql.com',
-    user: 'wIp1MpiKOt',
+    host: 'db4free.net',
+    user: 'dbaservice',
     password: 'pyiEqBNdgy',
-    database: 'wIp1MpiKOt',
+    database: 'databaseservice',
     connectionLimit: 10,
     multipleStatements: true
 });
