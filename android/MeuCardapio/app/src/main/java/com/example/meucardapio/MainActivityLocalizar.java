31ba122160ad51abf034ea3a64eee3527b9b7a3f
renatasantoso <renatasantosoliveira@gmail.com>
2020-05-01 21:33:41 -0300

    ALTERÇÃO DE CLICK NO MENU

---
+++ android/MeuCardapio/app/src/main/java/com/example/meucardapio/MainActivityLocalizar.java
@@ -0,0 +1,14 @@
+package com.example.meucardapio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityLocalizar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_localizar);
+    }
+}
--- android/MeuCardapio/app/src/main/java/com/example/meucardapio/MainActivityPrincipal.java
+++ android/MeuCardapio/app/src/main/java/com/example/meucardapio/MainActivityPrincipal.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivityPrincipal extends AppCompatActivity {
@@ -16,29 +17,29 @@ public class MainActivityPrincipal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_principal);
 
-        final TextView abrirComanda = findViewById(R.id.abrirComanda);
-        final TextView abrirCardapio = findViewById(R.id.abrirCardapio);
-        final TextView abrirCupom = findViewById(R.id.abrirCupom);
-        final TextView abrirCarteira = findViewById(R.id.abrirCarteira);
-        final TextView abrirAjuda = findViewById(R.id.abrirAjuda );
-        final ImageView abrirHelp = findViewById(R.id.abrirHelp );
+        final LinearLayout abrirComanda = findViewById(R.id.abrirComanda);
+        final LinearLayout abrirCardapio = findViewById(R.id.abrirCardapio);
+        final LinearLayout abrirCupom = findViewById(R.id.abrirCupom);
+        final LinearLayout abrirCarteira = findViewById(R.id.abrirCarteira);
+        final LinearLayout abrirAjuda = findViewById(R.id.abrirAjuda);
+        final LinearLayout abrirLocalizar = findViewById(R.id.abrirLocalizar);
 
 
 
 
         abrirComanda.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intentTelaCadastro = new Intent(getApplicationContext(), MainActivityPrincipalUsuario.class );
-                startActivity(intentTelaCadastro);
+               public void onClick(View v) {
+               Intent intentTelaCadastro = new Intent(getApplicationContext(), MainActivityPrincipalUsuario.class );
+               startActivity(intentTelaCadastro);
 
             }
         });
 
         abrirCardapio.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intentTelaCardapio = new Intent(getApplicationContext(), MainActivityCardapio.class );
+           public void onClick(View v) {
+               Intent intentTelaCardapio = new Intent(getApplicationContext(), MainActivityCardapio.class );
                 startActivity(intentTelaCardapio);
 
             }
@@ -46,18 +47,17 @@ public class MainActivityPrincipal extends AppCompatActivity {
         abrirCupom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intentTelaCupom = new Intent(getApplicationContext(), MainActivityCupom.class );
+               Intent intentTelaCupom = new Intent(getApplicationContext(), MainActivityCupom.class );
                 startActivity(intentTelaCupom);
             }
         });
         abrirCarteira.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intentTelaCarteira = new Intent(getApplicationContext(), MainActivityCarteira.class );
-                startActivity(intentTelaCarteira);
-            }
+               Intent intentTelaCarteira = new Intent(getApplicationContext(), MainActivityCarteira.class );
+               startActivity(intentTelaCarteira);
+           }
         });
-
         abrirAjuda.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -65,11 +65,12 @@ public class MainActivityPrincipal extends AppCompatActivity {
                 startActivity(intentTelaAjuda);
             }
         });
-        abrirHelp.setOnClickListener(new View.OnClickListener() {
+
+        abrirLocalizar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intentTelaAjuda = new Intent(getApplicationContext(), MainActivityAjuda.class );
-                startActivity(intentTelaAjuda);
+                Intent intentTelaLocalizar = new Intent(getApplicationContext(), MainActivityLocalizar.class );
+                startActivity(intentTelaLocalizar);
             }
         });
 
