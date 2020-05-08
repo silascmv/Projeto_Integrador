cc8c79d3584228a85f1bd0cf6c46df0cac2ce46f
jose.silas.c.vieira@accenture.com <jose.silas.c.vieira@accenture.com>
2020-05-08 20:36:55 -0300

    Ajustes API

--- service/dal.js
+++ service/dal.js
@@ -254,6 +254,7 @@ class DataAcessLayer {
                         console.log('Resultado : ' + results.length)
                         if(results.length == 0){
 
+                            console.log("Entrou no IF")
                             var resultado_mesa_ocupada = JSON.parse('{"status":"A mesa está ocupada/reservada no momento.","code_status":"01"}');
                             resolve(resultado_mesa_ocupada);
 
