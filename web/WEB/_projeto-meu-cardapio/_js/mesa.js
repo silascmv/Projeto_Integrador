9566a00c29938dc397b81a9366325a33ffb655c0
marciojohannes-correia <johannesmoura@yahoo.com.br>
2020-05-19 17:12:04 -0300

    js da mesa

--- web/WEB/_projeto-meu-cardapio/_js/mesa.js
+++ web/WEB/_projeto-meu-cardapio/_js/mesa.js
@@ -1,8 +1,8 @@
 function cadastroMesa() {
-    
+
     const DESCRICAO = $('#NOME_MESA').val();
-    const SN_ATIVO  = $('#SN_ATIVO:checked').length;
-    const QR_CODE   = $('#qrcodeMesa>img').attr('src');
+    const SN_ATIVO = $('#SN_ATIVO:checked').length;
+    const QR_CODE = $('#qrcodeMesa>img').attr('src');
 
     const formData = new FormData();
     formData.append('NOME_MESA', DESCRICAO)
@@ -14,4 +14,25 @@ function cadastroMesa() {
         body: formData
 
     }).then(response => response.json()).then(data => alert(data.status)).then(clearModal()).then(reload());
+}
+
+function dataURItoBlob(dataURI, type) {
+    // convert base64 to raw binary data held in a string
+    var byteString = atob(dataURI.split(',')[1]);
+
+    // separate out the mime component
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+    // write the bytes of the string to an ArrayBuffer
+    var ab = new ArrayBuffer(byteString.length);
+    var ia = new Uint8Array(ab);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+        // write the ArrayBuffer to a blob, and you're done
+    }
+
+    var bb = new Blob([ab], {
+        type: type
+    });
+    return bb;
 }
