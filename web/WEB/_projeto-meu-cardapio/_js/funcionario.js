6e436d9bc74d0e678847fc3178192243f26b0414
fabio silva <fabio.bernardo96@gmail.com>
2020-06-01 14:41:01 -0300

    Api Funcionarios

--- web/WEB/_projeto-meu-cardapio/_js/funcionario.js
+++ web/WEB/_projeto-meu-cardapio/_js/funcionario.js
@@ -8,19 +8,55 @@ function cadastro() {
     const SETOR = document.getElementsByName('SETOR')[0].value;
     const SN_ATIVO = $('#SN_ATIVO:checked').length;
 
-    const formData = new FormData();
-    formData.append('CD_LOGIN', CD_LOGIN)
-    formData.append('CD_SENHA', CD_SENHA)
-    formData.append('TIPO_USUARIO', TIPO_USUARIO)
-    formData.append('NOME_FUNCIONARIO', NOME_FUNCIONARIO)
-    formData.append('CPF', CPF)
-    formData.append('TELEFONE', TELEFONE)
-    formData.append('SETOR', SETOR)
-    formData.append('SN_ATIVO', SN_ATIVO)
+    // const formData = new FormData();
+    // formData.append('CD_LOGIN', CD_LOGIN)
+    // formData.append('CD_SENHA', CD_SENHA)
+    // formData.append('TIPO_USUARIO', TIPO_USUARIO)
+    // formData.append('NOME_FUNCIONARIO', NOME_FUNCIONARIO)
+    // formData.append('CPF', CPF)
+    // formData.append('TELEFONE', TELEFONE)
+    // formData.append('SETOR', SETOR)
+    // formData.append('SN_ATIVO', SN_ATIVO)
 
-    fetch('http://app-84c469d6-9c06-4181-9a74-5d84696798cf.cleverapps.io/addFuncionario/', {
+    // fetch('http://app-84c469d6-9c06-4181-9a74-5d84696798cf.cleverapps.io/addFuncionario/', {
+    //     method: 'POST',
+    //     body: formData
+
+    // }).then(response => response.json()).then(data => alert(data.status)).then(clearModal()).then(reload());
+
+    const rawResponse = await fetch('http://app-84c469d6-9c06-4181-9a74-5d84696798cf.cleverapps.io/addFuncionario/', {
         method: 'POST',
-        body: formData
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'CD_LOGIN': CD_LOGIN,
+            'CD_SENHA': CD_SENHA,
+            'TIPO_USUARIO': TIPO_USUARIO,
+            'NOME_FUNCIONARIO': NOME_FUNCIONARIO,
+            'CPF': CPF,
+            'TELEFONE': TELEFONE,
+            'SETOR': SETOR,
+            'SN_ATIVO': SN_ATIVO
+        })
+    });
 
-    }).then(response => response.json()).then(data => alert(data.status)).then(clearModal()).then(reload());
+    // fetch('http://app-84c469d6-9c06-4181-9a74-5d84696798cf.cleverapps.io/addFuncionario/', {
+    //     method: 'POST',
+    //     headers: {
+    //       'Accept': 'application/json',
+    //       'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify({
+    //         'CD_LOGIN': CD_LOGIN,
+    //         'CD_SENHA': CD_SENHA,
+    //         'TIPO_USUARIO': TIPO_USUARIO,
+    //         'NOME_FUNCIONARIO': NOME_FUNCIONARIO,
+    //         'CPF': CPF,
+    //         'TELEFONE': TELEFONE,
+    //         'SETOR': SETOR,
+    //         'SN_ATIVO': SN_ATIVO
+    //     }),
+    //     }).then(response => response.json()).then(data => alert(data.status)).then(clearModal()).then(reload());
 }
