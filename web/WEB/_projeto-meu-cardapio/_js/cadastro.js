7c680498c8b2116c18019e1c169991e3e393c90c
fabio silva <fabio.bernardo96@gmail.com>
2020-06-01 15:05:45 -0300

    cadastro correção

--- web/WEB/_projeto-meu-cardapio/_js/cadastro.js
+++ web/WEB/_projeto-meu-cardapio/_js/cadastro.js
@@ -1,3 +1,21 @@
+// function cadastro() {
+//   const username = document.getElementsByName('username')[0].value;
+//   const your_email = document.getElementsByName('your_email')[0].value;
+//   const password = document.getElementsByName('password')[0].value;
+//   const name = document.getElementsByName('name')[0].value;
+//   const address = document.getElementsByName('address')[0].value;
+//   const telephone = document.getElementsByName('telephone')[0].value;
+//   const cellphone = document.getElementsByName('cellphone')[0].value;
+//   fetch('http://app-84c469d6-9c06-4181-9a74-5d84696798cf.cleverapps.io/addCliente/?cd_login=' + username + '&cd_senha=' + password + '&nome=' + name + '&email=' + your_email + '&endereco=' + address + '&telefone=' + telephone + '&celular=' + cellphone, {
+//     method: 'POST',
+//     headers: {
+//       'Accept': 'application/json',
+//       'Content-Type': 'application/json'
+//     },
+
+//   }).then(response => response.json()).then(data => alert(data.status));
+// }
+
 function cadastro() {
   const username = document.getElementsByName('username')[0].value;
   const your_email = document.getElementsByName('your_email')[0].value;
@@ -6,12 +24,20 @@ function cadastro() {
   const address = document.getElementsByName('address')[0].value;
   const telephone = document.getElementsByName('telephone')[0].value;
   const cellphone = document.getElementsByName('cellphone')[0].value;
-  fetch('http://app-84c469d6-9c06-4181-9a74-5d84696798cf.cleverapps.io/addCliente/?cd_login=' + username + '&cd_senha=' + password + '&nome=' + name + '&email=' + your_email + '&endereco=' + address + '&telefone=' + telephone + '&celular=' + cellphone, {
+  fetch('http://app-84c469d6-9c06-4181-9a74-5d84696798cf.cleverapps.io/addCliente/', {
     method: 'POST',
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
-
+    body: JSON.stringify({
+      username: username, 
+      your_email: your_email,
+      password: password,
+      name: name,
+      address: address,
+      telephone: telephone,
+      cellphone: cellphone
+    }),
   }).then(response => response.json()).then(data => alert(data.status));
 }
