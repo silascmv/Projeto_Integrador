925d636c367bb110612f1f45889d54b81a0b442a
marciojohannes-correia <johannesmoura@yahoo.com.br>
2020-06-21 12:50:06 -0300

    Correção do bootstrap

--- web/WEB/_projeto-meu-cardapio/_js/login.js
+++ web/WEB/_projeto-meu-cardapio/_js/login.js
@@ -51,21 +51,21 @@ function login() {
 
 
   fetch('http://app-ee0cc445-4a89-42ba-8fe5-8954b141f3e2.cleverapps.io/realizarLoginFuncionario', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-          CD_LOGIN: CD_LOGIN,
-          CD_SENHA: CD_SENHA,
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      CD_LOGIN: CD_LOGIN,
+      CD_SENHA: CD_SENHA,
 
-      }),
-    }).then(response => response.json()).then(data => {
-      if (data.code_status === 1) {
-        localStorage.setItem('cardapio', true), window.location.href = "./index.html";
-      } else {
-        alert(data.status)
-      }
-    });
-  }
+    }),
+  }).then(response => response.json()).then(data => {
+    if (data.code_status === 1) {
+      localStorage.setItem('cardapio', true), window.location.href = "./index.html";
+    } else {
+      alert(data.status)
+    }
+  });
+}
